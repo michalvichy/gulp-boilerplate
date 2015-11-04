39b78cd2ba0f21ff02fe5faf811d9483b2bf67bc
Michał Skóra <michal.skora@chilid.pl>
2015-11-04 09:14:03 +0100

    New structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,40 +9,40 @@ var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
 
 gulp.task('scss', function() {
-    gulp.src('web/assets/scss/main.scss')
+    gulp.src('src/scss/main.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('web/assets/css'));
+        .pipe(gulp.dest('app/assets/css'));
 });
 
 gulp.task('jshint', function() {
-    gulp.src(['web/assets/js/main.js', 'web/assets/js/components/*.js'])
+    gulp.src(['src/js/main.js', 'src/js/components/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 gulp.task('scripts', function() {
-    gulp.src(['web/assets/js/components/*.js', 'web/assets/js/main.js'])
+    gulp.src(['src/js/components/*.js', 'src/js/main.js'])
         .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(concat('scripts.js'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('web/assets/js/'));
+        .pipe(gulp.dest('app/assets/js/'));
 });
 
 gulp.task('vendors', function() {
     gulp.src([
-        'web/assets/vendors/jquery/dist/jquery.min.js'
+        'bower_components/jquery/dist/jquery.min.js'
     ])
     .pipe(concat('vendors.js'))
-    .pipe(gulp.dest('web/assets/js/'));
+    .pipe(gulp.dest('app/assets/js/'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('web/assets/js/**/*.js', ['jshint', 'scripts', 'vendors']);
-  gulp.watch('web/assets/scss/**/*.scss', ['scss']);
+  gulp.watch('src/js/**/*.js', ['jshint', 'scripts', 'vendors']);
+  gulp.watch('src/scss/**/*.scss', ['scss']);
 });
 
 gulp.task('default', function() {
